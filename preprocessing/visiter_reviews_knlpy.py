4a17f4689fd9d102b8cdc20add541707fff54c5b
jieun <najee0610@naver.com>
2023-12-07 07:04:16 +0900

    방문자 리뷰 명사,형용사 추출(연남, 망원, 한남, 성수, 연희)

---
+++ preprocessing/visiter_reviews_knlpy.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from konlpy.tag import Okt
+import os
+
+# Okt 객체 생성
+okt = Okt()
+current_dir = os.getcwd()
+
+# 파일 경로 설정
+# filepath = os.path.join(current_dir, 'data', 'visiter_reviews', 'visiter_reviews_mangwon.csv')
+# filepath = os.path.join(current_dir, 'data', 'visiter_reviews', 'visiter_reviews_hannam.csv')
+# filepath = os.path.join(current_dir, 'data', 'visiter_reviews', 'visiter_reviews_seongsu.csv')
+# filepath = os.path.join(current_dir, 'data', 'visiter_reviews', 'visiter_reviews_yeonhee.csv')
+filepath = os.path.join(current_dir, 'data', 'visiter_reviews', 'visiter_reviews_yeonnam.csv')
+
+
+# csv 파일 읽기
+data = pd.read_csv(filepath, index_col=0)
+
+# 형태소 분석 결과를 저장할 DataFrame 생성
+morphs_data = pd.DataFrame(index=data.index, columns=data.columns)
+
+# 형태소 분석 및 품사 태깅
+for column in data.columns:
+    for idx in data.index:
+        text = data.loc[idx, column]
+        if isinstance(text, str):
+            morphs = [word for word, tag in okt.pos(okt.normalize(text), stem=True) if tag in ['Noun', 'Adjective']]
+            morphs_data.loc[idx, column] = ' '.join(morphs)
+
+# 결과 확인
+print(morphs_data.head())
+
+# 결과를 새로운 csv 파일로 저장
+# morphs_data.to_csv('visiter_reviews_knlpy_mangwon.csv')
+# morphs_data.to_csv('visiter_reviews_knlpy_hannam.csv')
+# morphs_data.to_csv('visiter_reviews_knlpy_seongsu.csv')
+# morphs_data.to_csv('visiter_reviews_knlpy_yeonhee.csv')
+morphs_data.to_csv('visiter_reviews_knlpy_yeonnam.csv')
